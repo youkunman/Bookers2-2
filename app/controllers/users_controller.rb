810aecc858e8382ebcbdb2b88d19fb97548bcced
Your Name <you@example.com>
2019-02-23 12:55:44 +0000

    sorry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!, except: [:about, :new]
+before_action :authenticate_user!, except: [:about, :new, :edit]
   def about
 
   end
